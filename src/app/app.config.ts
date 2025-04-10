17689eddec7238efee98dd6461822936c2128a6f
Your GitHub Username <your.email@example.com>
2025-04-10 13:52:11 -0400

    Removing and adding new API key

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 import {
@@ -10,22 +11,13 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCHFGcEhZzsyUsFB3fT_K0xgSbXVA-jOfs',
-  authDomain: 'loan-publishing.firebaseapp.com',
-  projectId: 'loan-publishing',
-  storageBucket: 'loan-publishing.firebasestorage.app',
-  messagingSenderId: '833676109932',
-  appId: '1:833676109932:web:daac2db45f8335c4b75049',
-};
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()), // Add this line to provide the Auth service
+    provideAuth(() => getAuth()), // Auth service
   ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '', // Leave empty in committed code
+    authDomain: 'loan-publishing.firebaseapp.com',
+    projectId: 'loan-publishing',
+    storageBucket: 'loan-publishing.firebasestorage.app',
+    messagingSenderId: '833676109932',
+    appId: '1:833676109932:web:daac2db45f8335c4b75049',
+  },
+};
