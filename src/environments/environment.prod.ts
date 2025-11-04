db192bc32dce475b17c3854c8fa4bca358ce40bb
Michael-CLF <mschlotthauer@iloanfund.com>
2025-11-04 13:25:51 -0500

    Fixed wrong messenger ID in environments file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -20,7 +20,7 @@ export const environment = {
     authDomain: 'loanpub.firebaseapp.com',
     projectId: 'loanpub',
     storageBucket: 'loanpub.firebasestorage.app',
-    messagingSenderId: '837394099537',
+    messagingSenderId: '837394993537',
     appId: '1:837394993537:web:3f2e4f08938640d074d880', 
     measurementId: 'G-MPD1MGM3WJ',
   },
